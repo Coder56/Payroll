cb7d56683205949451736bcd292c15e85a057593
coder56 <iamankit.tiwari13@gmail.com>
2020-10-05 13:24:04 +0530

    Initial commit

---
+++ MonthlyPayslip.Generator/IncomeTax/TaxTableGenerator.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace MonthlyPayslip.Calculator.IncomeTax
+{
+    public class TaxTableGenerator
+    {
+        public List<TaxTable> TaxTableData { get; }
+
+        public TaxTableGenerator()        {
+
+            TaxTableData = new List<TaxTable>()
+           {
+                
+                
+                TaxTable.Add(0, 18200, 0, 0m),
+                TaxTable.Add(18201, 37000, 0, 0.19m),
+                TaxTable.Add(37001, 87000, 3572, 0.325m),
+                TaxTable.Add(87001, 180000, 17547, 0.37m),
+                TaxTable.Add(180001, decimal.MaxValue,54547, 0.45m),
+           };
+        }          
+    }
+}
